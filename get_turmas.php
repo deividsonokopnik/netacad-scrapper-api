3512b5abb56d7cfbd955d5fa7d2983d650db5dba
Deividson Luiz Okopnik <86014741+deividsonokopnik@users.noreply.github.com>
2022-03-11 11:35:46 -0300

    Criação da Base da API de listagem de turmas

--- get_turmas.php
+++ get_turmas.php
@@ -1,7 +1,5 @@
 <?php
 include 'config.php';
-// $con=mysql_connect("$host","$username","$password") or die("Server Error");
-// mysql_select_db("$database") or die("Database error");
 $con = new mysqli("$host", "$username", "$password", "$database") or die("Server Error");
 
 if($con==true)
@@ -11,8 +9,8 @@ $res = $con->query("select id, nome from turmas");
 if ($res) {
    $rows=$res->fetch_all(MYSQLI_ASSOC);
    $row = $res->fetch_row();
+//   echo (base64_encode(json_encode($rows)));
    echo (json_encode($rows));
-//    echo "$row[0] - $row[1]";
 }
 }
 else
